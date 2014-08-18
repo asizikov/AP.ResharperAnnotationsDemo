c3a9327fb878d2d9af4b47503469cf9bdf8fc454
Anton Sizikov <anton.sizikov@albumprinter.com>
2014-08-18 13:46:20 +0200

    String format demo added

---
+++ Annotations.Demo/06_StringFormatDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using JetBrains.Annotations;
+using NUnit.Framework;
+
+namespace Annotations.Demo
+{
+    [TestFixture]
+    public class StringFormatDemo
+    {
+        [Test]
+        public void Test()
+        {
+            DemoLoger.PrintMessage("hello world. It's {0} now {33}", DateTime.Now, 12);
+        }
+    }
+
+    
+    public static class DemoLoger
+    {
+//        [StringFormatMethod("formatTemplate")]
+        public static void PrintMessage(string formatTemplate, params object[] args)
+        {
+            Debug.WriteLine(formatTemplate, args);
+        }
+    }
+}
